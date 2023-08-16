94df3aab5a2b253ea5853075c04c9eff3068abef
Anton Uzhva <anton.itec91@gmail.com>
2023-08-16 19:59:02 +0300

    Do  all requirements due this task

---
+++ src/main/java/org/example/OnlineBookStoreApp.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.math.BigDecimal;
+import org.example.model.Book;
+import org.example.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(scanBasePackages = "org.example")
+public class OnlineBookStoreApp {
+    @Autowired
+    private BookService bookService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreApp.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setAuthor("Shild");
+            book.setIsbn("234456");
+            book.setPrice(BigDecimal.TEN);
+            book.setTitle("Java 8");
+            bookService.save(book);
+        };
+    }
+}
---
+++ src/main/java/org/example/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,51 @@
+package org.example.repository.impl;
+
+import java.util.List;
+import org.example.exception.DataProcessingException;
+import org.example.model.Book;
+import org.example.repository.BookRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save a book : " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Book> query = session.createQuery("FROM Book b", Book.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find all books : ", e);
+        }
+    }
+}
