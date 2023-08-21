e69ff70e11f9ccaad348782cfaab6a1267cf58c8
Anton Uzhva <anton.itec91@gmail.com>
2023-08-21 13:10:36 +0300

    Implement GlobalExceptionHandler

--- src/main/java/org/example/model/Book.java
+++ src/main/java/org/example/model/Book.java
@@ -7,6 +7,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PositiveOrZero;
 import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
@@ -20,14 +23,23 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
+    @NotEmpty
     private String title;
+
     @Column(nullable = false)
+    @NotEmpty
     private String author;
+
     @Column(unique = true, nullable = false)
+    @NotEmpty
     private String isbn;
+
     @Column(nullable = false)
+    @PositiveOrZero
     private BigDecimal price;
+
     private String description;
     private String coverImage;
     private boolean isDeleted;
