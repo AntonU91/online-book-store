47ce5ff9765a0b75d73d5bc96286df95cec04219
Anton Uzhva <anton.itec91@gmail.com>
2023-09-02 19:35:22 +0300

    Initial commit

---
+++ src/main/java/org/example/model/Order.java
@@ -0,0 +1,56 @@
+package org.example.model;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+import lombok.Data;
+
+@Entity
+@Table(name = "orders")
+@Data
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false)
+    private BigDecimal total;
+
+    @Column(name = "order_date", nullable = false)
+    private LocalDateTime orderDate;
+
+    @Column(name = "shipping_address", nullable = false)
+    private String shippingAddress;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.REMOVE)
+    private Set<OrderItem> orderItems;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+
+    public enum Status {
+        COMPLETED, DELIVERED, PINNED
+    }
+
+}
